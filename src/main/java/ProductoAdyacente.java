ee23e0f17337e58c5f467c24600bf92cfbf7fa44
chris <c.gajardo08@ufromail.cl>
2023-09-20 09:48:50 -0300

    Avance 1 ProductoAdyacente

---
+++ src/main/java/ProductoAdyacente.java
@@ -0,0 +1,11 @@
+public class ProductoAdyacente {
+    public static int productoAdyacente (int[] array){
+        int ProductoMayor = -1000;
+        for (int indice = 0; indice < (array.length-1); indice++){
+            if ((array[indice]*array[indice+1])>ProductoMayor){
+                ProductoMayor = (array[indice]*array[indice+1]);
+            }
+        }
+        return ProductoMayor;
+    }
+}
