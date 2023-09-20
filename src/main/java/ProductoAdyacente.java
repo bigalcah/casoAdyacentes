4e156cf0f99540e61abd030d87e86a4f32fc77ce
chris <c.gajardo08@ufromail.cl>
2023-09-20 09:58:25 -0300

    Avance 1 ProductoAdyacente

--- src/main/java/ProductoAdyacente.java
+++ src/main/java/ProductoAdyacente.java
@@ -1,12 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ProductoAdyacente {
-    public static int productoAdyacente (int[] array){
-        int ProductoMayor = -1000;
-        for (int indice = 0; indice < (array.length-1); indice++){
-            if ((array[indice]*array[indice+1])>ProductoMayor){
-                ProductoMayor = (array[indice]*array[indice+1]);
-            }
-        }
-        return ProductoMayor;
+
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String [] args) {}
+
+    public static int[] generarArray() {
+
+        int largo;
+
+        do {
+
+            System.out.print("Ingrese el largo de la lista: ");
+            largo = leer.nextInt();
+
+        } while (!validarLenght(largo));
+
+        return new int[largo];
     }
-    //LIsto para merge
+
+    public static void llenarArray(int [] array){
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = new Random(). nextInt(-1000,1000);
+        }
+
+    }
+
+    public static boolean validarLenght(int largo){
+
+        if (largo < 2 || largo > 20) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
