7c0daa7587a75199fa7eb0317d0aefb397d88794
jfaundez07 <j.faundez07@ufromail.cl>
2023-09-20 09:15:54 -0300

    Primer avance generar array

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -1,7 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class ProductoAdyacentes {
+
+    public static Scanner leer;
 
     public static int productoAdayacentes(){
         return 0;
     }
+
+    public static int[] generarArray() {
+        System.out.println("Ingrese el largo de la lista");
+        int largo = leer.nextInt();
+        int [] array = new int[largo];
+        return array;
+    }
+
+    public static int llenarArray(int [] array){}
 
 }
