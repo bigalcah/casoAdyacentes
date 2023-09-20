db749fe9c7ebd72da1c89ff1908d014c7a6bf794
Javier Notebook <j.alcalde01@ufromail.cl>
2023-09-20 09:50:17 -0300

    primer commit

---
+++ src/main/java/ProductoAdyacente.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class ProductoAdyacente {
+    public static Scanner leer = new Scanner(System.in);
+    public static void main(String [] args) {
+
+        int [] array = generarArray();
+        llenarArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+    public static int productoAdayacentes(){
+        return 0;
+    }
+    public static int[] generarArray() {
+
+        int largo;
+
+        do {
+
+            System.out.print("Ingrese el largo de la lista: ");
+            largo = leer.nextInt();
+
+        } while (!validarLenght(largo));
+
+        return new int[largo];
+    }
+    public static void llenarArray(int [] array){
+        Random numeroRandom = new Random();
+        for(int i = 0; i < array.length; i++) {
+            array[i] = numeroRandom.nextInt(-1000,1000);
+        }
+    }
+    public static boolean validarLenght(int largo){
+        if (largo < 2 || largo > 20) {
+            return false;
+        }
+        return true;
+    }
+}
+
