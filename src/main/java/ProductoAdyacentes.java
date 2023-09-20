fe40a43e9d7a20e1a27266ed71abe5769574c8be
jfaundez07 <j.faundez07@ufromail.cl>
2023-09-20 09:26:04 -0300

    Segundo avance llenar array

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -1,22 +1,34 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class ProductoAdyacentes {
 
-    public static Scanner leer;
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String [] args) {
+
+        int [] array = generarArray();
+        llenarArray(array);
+        System.out.println(Arrays.toString(array));
+    }
 
     public static int productoAdayacentes(){
         return 0;
     }
 
     public static int[] generarArray() {
+
         System.out.println("Ingrese el largo de la lista");
         int largo = leer.nextInt();
-        int [] array = new int[largo];
-        return array;
+        return new int[largo];
     }
 
-    public static int llenarArray(int [] array){}
+    public static void llenarArray(int [] array){
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = new Random(). nextInt(-1000,1000);
+        }
+    }
 
 }
