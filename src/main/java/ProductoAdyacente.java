81fc4c203dc2ea2f90eff882208832fb65d45215
jfaundez07 <j.faundez07@ufromail.cl>
2023-09-20 10:12:02 -0300

    Merge 2 Cris-Joaquin

--- src/main/java/ProductoAdyacente.java
+++ src/main/java/ProductoAdyacente.java
@@ -36,6 +36,7 @@ public class ProductoAdyacente {
     }
 
     public static int productoAdyacente (int[] array){
+
         int ProductoMayor = 0;
         for (int indice = 0; indice < (array.length-1); indice++){
             if ((array[indice]*array[indice+1])>ProductoMayor || indice == 0){
